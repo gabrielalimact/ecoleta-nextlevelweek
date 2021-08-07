96f50982a3904ea287ba3f88bf9f41882a8903af
gabrielalimact <leticiacena70@gmail.com>
2021-08-07 16:12:27 -0300

    update

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
@@ -11,17 +10,3 @@ close.addEventListener("click", () => {
     modal.classList.add("hide")
 })
 
-=======
-const buttonSearch = document.querySelector("#page-home main a")
-const modal = document.querySelector("#modal")
-const close = document.querySelector("#modal .header a")
-
-buttonSearch.addEventListener("click", () => {
-    modal.classList.remove("hide")
-})
-
-close.addEventListener("click", () => {
-    modal.classList.add("hide")
-})
-
->>>>>>> 8543901dac5d4b59742754627e66afdd6f9e5213
